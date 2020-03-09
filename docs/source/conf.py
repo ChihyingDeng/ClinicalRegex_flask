db3ec11b54248d263cdeec335e04471d1c081813
Chih-Ying Deng <chih-yingdeng@chih-yings-imac.dfci.harvard.edu>
2020-03-09 17:02:25 -0400

    conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,21 +30,15 @@ author = 'Chih-Ying Deng'
 extensions = [
 ]
 
-# Add any paths that contain templates here, relative to this directory.
+import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
